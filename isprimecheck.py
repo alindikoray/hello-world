650b30dbeb14f90fc523e7cf55f9d3cc006619ae
alindikoray <alindikris77@gmail.com>
2016-10-31 10:39:11 +0300

    Update isprimecheck.py

--- isprimecheck.py
+++ isprimecheck.py
@@ -1,6 +1,13 @@
-def isprime():
-	for i in range(2, int(n**0.5)+1):
-		n%i==0
-		return false
+max_value = eval(input ("display numbers to what maximum value?"))
+for value in range(2, max_value +1):
+	is_prime = True
+	# Try all possible factors from 2 to value - 1
+	for trial_factorin range (2, value):
+		if value % trial_factor == 0:
+			is_prime = False # Found a factor
+			break           # No need to continue; it is NOT prime 
+	if is_prime:
+		print(value, end ' ')
+print()
 		
 
